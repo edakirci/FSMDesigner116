b24307469b9650d2138a1e7d32e27f3beecf33df
efekaynarcali <154628062+efekaynarcali@users.noreply.github.com>
2025-05-02 22:25:29 +0300

    adding command processor class

--- src/CommandProcessor.java
+++ src/CommandProcessor.java
@@ -1,2 +1,12 @@
 public class CommandProcessor {
+    private final DeterministicFSM fsm;
+    private final FileManager fileManager;
+    public CommandProcessor(DeterministicFSM fsm) {
+        this.fsm = fsm;
+        this.fileManager = new FileManager();
+    }
+    public void processCommand(String command,int lineNum) {
+        fsm.processRawCommand(command,lineNum);
+    }
 }
+ 
