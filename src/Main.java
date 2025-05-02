f0a334cbfa8e1c92b2907e54474c902378cb1690
oykudabak <153430247+oykudabak@users.noreply.github.com>
2025-05-02 22:38:17 +0300

    adding main class

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,19 @@
+import java.io.*;
 public class Main {
+
+    class Logger {
+        private PrintWriter writer;
+        private boolean enabled;
+
+        public void startLogging(String filename) {
+            stopLogging();
+            try {
+                writer = new PrintWriter(new FileWriter(filename, false));
+                enabled = true;
+            } catch (IOException e) {
+                System.out.println("Error: cannot create log file " + filename);
+                enabled = false;
+            }
+        }
+    }
 }
