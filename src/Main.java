364fe6634ed2258a0d4eb5e0a885b4913c097709
oykudabak <153430247+oykudabak@users.noreply.github.com>
2025-05-02 22:54:09 +0300

    fixed the main

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,25 @@
-import java.io.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        DeterministicFSM fsm = new DeterministicFSM();
+        CommandProcessor processor = new CommandProcessor(fsm);
 
-    class Logger {
-        private PrintWriter writer;
-        private boolean enabled;
-
-        public void startLogging(String filename) {
-            stopLogging();
-            try {
-                writer = new PrintWriter(new FileWriter(filename, false));
-                enabled = true;
-            } catch (IOException e) {
-                System.out.println("Error: cannot create log file " + filename);
-                enabled = false;
-            }
+        if (args.length == 1) {
+            fsm.load(args[0]);
+            return;
         }
+
+        String versionNo = "1.0";
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("MMMM d, yyyy, HH:mm");
+        System.out.println("FSM DESIGNER " + versionNo + " " + fmt.format(now));
+
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder cmdBuffer = new StringBuilder();
+        System.out.print("? ");
     }
+
 }
